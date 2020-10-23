c8b74603653610544403bf3aafb4275dc68f9de9
Oleksandr Hubanov <alex@0x0h.com>
2020-10-23 12:13:19 +0300

    std::unordered_map suppose to have O(1) complexity, but in case of too many collisions it will fall to O(N), while std::map always has O(log N). log 1 = 1 and log 1000000 is 6, is it comparably quick? need to run some tests with time measuring, but its not included in task.

--- RocketSoftwareInterview/EfficientMemory/main.cpp
+++ RocketSoftwareInterview/EfficientMemory/main.cpp
@@ -1,6 +1,29 @@
 #include <iostream>
+#include <string>
+#include <map>
+
+std::map<std::string, int> userInputPool;
 
 int main()
 {
-    std::cout << "Please enter sequence\n";
+	std::cout << "Please enter sequence or ctrl+c to close\n";
+	while (true)
+	{
+		std::cout << "> ";
+		std::string userInputSTR;
+		std::getline(std::cin, userInputSTR);
+		std::cout << "Your input is: \'" << userInputSTR << "\' ";
+		std::map<std::string, int>::iterator matchingUserInput = userInputPool.find(userInputSTR);
+		if (matchingUserInput != userInputPool.end())
+		{
+			matchingUserInput->second++;
+			std::cout << "been stored for " << matchingUserInput->second << " times\n";
+		}
+		else
+		{
+			std::cout << "will be stored now\n";
+			userInputPool.insert(std::pair<std::string, int>(userInputSTR, 1));
+		}
+	}
+	return 0;
 }
